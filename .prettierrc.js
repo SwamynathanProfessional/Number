1763d815d4e258f03e78d78e034d569377d5ebcc
SwamynathanProfessional <swamynathan.bommaganty@gmail.com>
2021-07-14 21:13:22 +0530

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+};
